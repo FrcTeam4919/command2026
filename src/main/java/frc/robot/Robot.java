ff8c27b9ef62014655cf9e3ef5939a8c634b4e29
migeal <99619520+migeal@users.noreply.github.com>
2026-02-08 13:50:39 -0800

    changed back calls

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -58,8 +58,8 @@ public class Robot extends TimedRobot {
 
     // schedule the autonomous command (example)
     if (m_autonomousCommand != null) {
-     // CommandScheduler.getInstance().schedule(m_autonomousCommand);
-      m_autonomousCommand.execute();
+      CommandScheduler.getInstance().schedule(m_autonomousCommand);
+     
     }
   }
 
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -101,7 +101,7 @@ public class RobotContainer {
                  
           );
 
-           autoChooser = AutoBuilder.buildAutoChooser("ShootNload.auto");
+           autoChooser = AutoBuilder.buildAutoChooser();
            
   }
 
